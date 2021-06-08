a1492260d6243173e11a9a66e0265b433705f60c
koumoula <alex@koumoula.com>
2021-06-08 12:35:58 +0200

    add node services

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+export ABCDESKTOP_RUN_DIR='/composer/run'
+
 CONTAINER_IP_ADDR=$(ip route get 1 | awk '{print $7;exit}')
 echo "Container local ip addr is $CONTAINER_IP_ADDR"
 export CONTAINER_IP_ADDR
-
 
 # replace CONTAINER_IP_ADDR in listen for pulseaudio
 sed -i "s/localhost:631/$CONTAINER_IP_ADDR:631/g" /etc/cups/cupsd.conf 
@@ -11,5 +12,6 @@ if [ -f /tmp/krb5cc_4096 ]; then
 	# copy the krb5cc from ballon to root 
         cp /tmp/krb5cc_4096 /tmp/krb5cc_0
 fi
-/usr/sbin/cupsd -c /etc/cups/cupsd.conf -f
 
+# start supervisord
+/usr/bin/supervisord --pidfile /var/run/desktop/supervisord.pid --nodaemon --configuration /etc/supervisord.conf
