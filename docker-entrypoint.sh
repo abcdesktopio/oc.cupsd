8b99f2a48c66459a16ed6c73a5ba1e1158ca8351
Alexandre DEVELY <12896316+alexandredevely@users.noreply.github.com>
2021-06-14 19:05:04 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 export ABCDESKTOP_RUN_DIR='/composer/run'
 
-CONTAINER_IP_ADDR=$(ip route get 1 | awk '{print $7;exit}')
+CONTAINER_IP_ADDR=$(hostname -i)
 echo "Container local ip addr is $CONTAINER_IP_ADDR"
 export CONTAINER_IP_ADDR
 
